512a94b162e377fa5afce266f777a3edc6e9f3eb
ion <ionix@protonmail.com>
2016-09-30 09:57:49 +0200

    init commit

--- CFade/main.cpp
+++ CFade/main.cpp
@@ -2,16 +2,19 @@
 #include <iostream>
 #include <string>
 
-FadeAnimator fa = FadeAnimator(255, 150, 12);
-
 int main()
 {
+	FadeAnimator fa = FadeAnimator(255, 150, 12);
+	std::cin.get();
+	std::cout << "FadeInAnimation";
 	fa.FadeInAnimation();
 	std::cin.get();
+	std::cout << "FadeOutAnimation";
 	fa.FadeOutAnimation();
 	std::cin.get();
 	fa.FadeInOutAnimation(10, true);
 	std::string str;
 	getline(std::cin, str);
+
 	return 0;
 }
